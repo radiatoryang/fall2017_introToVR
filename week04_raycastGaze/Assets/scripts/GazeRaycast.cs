fb58e9235efabee6e6ef7835d89c6be5965f7111
Robert Yang <yang.robert.w@gmail.com>
2017-09-25 20:20:37 -0400

    week04: raycasts and gaze tracking... SimpleRaycast.cs, and GazeRaycast.cs

---
+++ week04_raycastGaze/Assets/scripts/GazeRaycast.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// put this script on your Main Camera
+public class GazeRaycast : MonoBehaviour {
+
+	public LayerMask myRayHitMask; // edit in Inspector
+	
+	void Update () {
+		// shoot a raycast based on the Main Camera's forward direction
+
+		// STEP 1: make a Ray object
+		Ray myRay = new Ray( transform.position, transform.forward );
+
+		// STEP 2: declare the distance
+		float myRayDistance = 20f; // only affect stuff 20 meters near us
+
+		// STEP 3: visualize the Raycast
+		Debug.DrawRay( myRay.origin, myRay.direction * myRayDistance, Color.yellow );
+
+		// STEP 3.5: setup a RaycastHit object, to hold RaycastHit data
+		RaycastHit myRayHit = new RaycastHit(); // this is just an empty container
+
+		// STEP 4: shoot the Raycast
+		if( Physics.Raycast( myRay, out myRayHit, myRayDistance, myRayHitMask ) ) {
+			Debug.Log( "I'm looking at " + myRayHit.collider.name ); // the thing the raycast hit
+
+			// we can now access anything on the collider that we hit (e.g. transform, gameObject)
+
+			// let's smoothly rotate the spheres to look toward us
+			myRayHit.transform.forward = Vector3.Lerp( 
+				myRayHit.transform.forward,
+				Vector3.Normalize( transform.position - myRayHit.transform.position ),
+				Time.deltaTime * 5f
+			);
+		}
+
+
+		// for testing purposes: code a simple mouse-look
+		transform.Rotate( -Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f );
+	}
+}
---
+++ week04_raycastGaze/Assets/scripts/SimpleRaycast.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRaycast : MonoBehaviour {
+
+	public Transform cube, sphere;
+	
+	// Update is called once per frame
+	void Update () {
+		// STEP 1: build our "Ray" object, based on the cube's transform
+		Ray myRay = new Ray( cube.position, cube.forward );
+		// "cube.forward" refers to the cube's local Z axis (blue)
+
+		// STEP 2: declare the raycast distance
+		float myRaycastDistance = 5f;
+
+		// STEP 3: (optional, but recommended:) visualize the ray in your Scene view
+		Debug.DrawRay( myRay.origin, myRay.direction * myRaycastDistance, Color.yellow );
+
+		// STEP 4: shoot our raycast now?
+		if( Physics.Raycast( myRay, myRaycastDistance ) ) { // TRUE == the raycast hit a collider
+			Debug.Log( "raycast hit something!" );
+			cube.Rotate( 0f, 1f, 0f ); // rotate the cube away
+		}
+	}
+}
