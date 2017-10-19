3c6b69bc70a9b9d9d28616c60561b783a062240f
Robert Yang <yang.robert.w@gmail.com>
2017-10-18 20:03:17 -0400

    week07: more controller code

---
+++ week07_moreControllers/Assets/scripts/InteractionButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Valve.VR.InteractionSystem;
+
+public class InteractionButton : MonoBehaviour {
+
+	// will happen automatically, every frame, as long as a Hand is touching it
+	void HandHoverUpdate (Hand hand) {
+		// is the player pulling the trigger on this Hand?
+		if( hand.GetStandardInteractionButtonDown() ) {
+			Debug.Log( "button pressed!" );
+			// for debug purposes, destroy myself (the button) when it's pressed
+			Destroy( this.gameObject );
+		}
+	}
+
+	// if you make a function "public void", that means UnityEvents can talk to it
+	// for example, like in InteractionButtonEvents script
+	public void DestroyThisButton () {
+		Debug.Log( "something happened!" );
+		Destroy( this.gameObject );
+	}
+
+}
---
+++ week07_moreControllers/Assets/scripts/MoreHandInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Valve.VR.InteractionSystem; // we need this to talk to Hands
+
+// put this script on a Hand; talk to the Hand to get more controller data
+public class MoreHandInput : MonoBehaviour {
+
+	Hand myHand;
+	SteamVR_Controller.Device myController { get { return myHand.controller; } }
+
+	// Use this for initialization
+	void Start () {
+		myHand = GetComponent<Hand>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if( myController != null ) { // is the controller connected?
+			// am I pulling my Analog Stick or Touchpad toward the right?
+			if( myController.GetAxis( Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad ).x > 0.5f ) {
+				Debug.Log( "right!" );
+			}
+		}
+	}
+}
